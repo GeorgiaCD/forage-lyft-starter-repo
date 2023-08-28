1d86377dc10c2641f02b81ee2163c0a31b0ca144
GeorgiaCD <georgiacrawforddellar@gmail.com>
2023-08-28 12:02:09 +0100

    Step 2: added comments to capulet_engine

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,7 +1,8 @@
 from abc import ABC
 
 from car import Car
-
+# CapuletEngine inherits from both Car and ABC (multiple inheritance)
+# constructor takes in three parameters: last_service_date, current_mileage, last_service_mileage
 
 class CapuletEngine(Car, ABC):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
@@ -9,5 +10,11 @@ class CapuletEngine(Car, ABC):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
+# method calculates whether the engine should be serviced based on mileage.
     def engine_should_be_serviced(self):
         return self.current_mileage - self.last_service_mileage > 30000
+
+# since inheritance needs a needs_service() method 
+# def needs_service(self):
+#         return self.engine_should_be_serviced()
+    
