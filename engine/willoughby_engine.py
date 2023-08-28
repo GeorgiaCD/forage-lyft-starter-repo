792448836e6528f6c0f40459fcf61792f1b6c533
GeorgiaCD <georgiacrawforddellar@gmail.com>
2023-08-28 12:12:32 +0100

    Step 2: added comments to willoughby_engine

--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -2,6 +2,8 @@ from abc import ABC
 
 from car import Car
 
+# inherits from Car and ABC
+# constructor takes in three parameters: last_service_date, current_mileage, last_service_mileage
 
 class WilloughbyEngine(Car, ABC):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
@@ -9,5 +11,6 @@ class WilloughbyEngine(Car, ABC):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
+# method engine_should_be_serviced if the difference between the two mileages is greater than 60000
     def engine_should_be_serviced(self):
         return self.current_mileage - self.last_service_mileage > 60000
